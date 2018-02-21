d49603c5a08abeba0611b74b3086eccf585ca6e4
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2018-02-21 23:59:48 +0100

    breakpoints and single-stepping

--- src/debugger.cpp
+++ src/debugger.cpp
@@ -11,7 +11,10 @@ using namespace Executor;
 namespace
 {
 bool mon_inited = false;
+bool singlestep = false;
 
+void InitDebugger();
+void EnterDebugger();
 
 void InitDebugger()
 {
@@ -21,6 +24,19 @@ void InitDebugger()
     mon_init();
     mon_read_byte = [](mon_addr_t addr) { return (uint32_t) *(uint8_t*)SYN68K_TO_US(addr); };
     mon_write_byte = [](mon_addr_t addr, uint32_t b) { *(uint8_t*)SYN68K_TO_US(addr) = (uint8_t) b; };
+
+    PowerCore& cpu = getPowerCore();
+    cpu.debugger = [](PowerCore& cpu) { EnterDebugger(); };
+    cpu.getNextBreakpoint = [](uint32_t addr) {
+        if(singlestep)
+            return addr;
+        
+        auto breakpoint_it = active_break_points.lower_bound(addr);
+        if(breakpoint_it == active_break_points.end())
+            return 0xFFFFFFFF;
+        else
+            return *breakpoint_it;
+    };
 
     mon_add_command("es", [] { ExitToShell(); }, "Exit To Shell");
 
@@ -48,12 +64,19 @@ void InitDebugger()
         
     }, "show ppc registers");
 
+    mon_add_command("s", [] {
+        mon_exit_requested = true;
+        singlestep = true;
+    }, "single step");
+
     mon_inited = true;
 }
 
 void EnterDebugger()
 {
     InitDebugger();
+    mon_dot_address = getPowerCore().CIA;
+    singlestep = false;
     const char *args[] = {"mon", "-m", "-r", nullptr};
     mon(3,args);
 }
