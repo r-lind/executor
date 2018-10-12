88fc80b961017a8c86d1218fe5d0eb0d3b1c7e9f
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2018-10-12 16:56:46 +0200

    redo mac resource fork support to properly truncate resource forks

--- src/localvolume/mac.cpp
+++ src/localvolume/mac.cpp
@@ -14,36 +14,71 @@ class MacResourceFork : public PlainDataFork
 public:
     using PlainDataFork::PlainDataFork;
 
+    virtual size_t getEOF() override;
     virtual void setEOF(size_t sz) override;
+    virtual size_t read(size_t offset, void *p, size_t n) override;
+    virtual size_t write(size_t offset, void *p, size_t n) override;
 };
+
+// NOTE:
+// The resource fork is available as filename/..namedfork/rsrc
+// as a more-or-less regular file.
+// However, on APFS volumes, ftruncate does not work on that file.
+// On Mojave, removexattr doesn't work while that file is open.
+// So, the code below opens the data fork and does everything
+// via the xattr API. There is no API for shortening an xattr,
+// but as ftruncate doesn't work anyway, and the maximum size of
+// a resource fork is 16MB, we just read the whole thing into RAM,
+// remove the attribute, and write it again at the desired size.
+
+size_t MacResourceFork::getEOF()
+{
+    errno = 0;
+    ssize_t sz = fgetxattr(fd, XATTR_RESOURCEFORK_NAME, nullptr, 0, 0, 0);
+    if(errno == ENOATTR)
+        return 0;
+    if(sz < 0)
+        throw OSErrorException(ioErr);
+    return size_t(sz);
+}
 
 void MacResourceFork::setEOF(size_t sz)
 {
-    ftruncate(fd, sz);
-    
-    // ftruncate does not work for resouce forks on APFS volumes on High Sierra.
-    size_t actual = lseek(fd, 0, SEEK_END);
+    errno = 0;
+    ssize_t curSz = fgetxattr(fd, XATTR_RESOURCEFORK_NAME, nullptr, 0, 0, 0);
+    if(curSz < 0 && errno != ENOATTR)
+        curSz = 0;
+    if(curSz < 0)
+        throw OSErrorException(ioErr);
 
-    // FIXME: ... and on Mojave, removexattr fails while the file is open.
-    if(actual > sz)
-    {
-        if(sz < 16 * 1024 * 1024)   // 16 MB is the maximum size for a resfork
-        {
-            std::string dfpath = path_.parent_path().parent_path().string();
+    if(sz > 16 * 1024 * 1024)
+        sz = 16 * 1024 * 1024;   // 16 MB is the maximum size for a resfork
 
-            void *buf = malloc(sz);
-            getxattr(dfpath.c_str(), "com.apple.ResourceFork", buf, sz,
-                    0, 0);
-            removexattr(dfpath.c_str(), "com.apple.ResourceFork", 0);
-            setxattr(dfpath.c_str(), "com.apple.ResourceFork", buf, sz,
-                    0, 0);
-            free(buf);
-        }
-    }
-    else if(actual < sz)
+    if(sz > curSz)
     {
-        pwrite(fd, "", 1, sz-1);
+        fsetxattr(fd, XATTR_RESOURCEFORK_NAME, "", 1, sz - 1, 0);
     }
+    else if(sz < curSz)
+    {
+        void *buf = malloc(sz);
+        fgetxattr(fd, XATTR_RESOURCEFORK_NAME, buf, sz, 0, 0);
+        fremovexattr(fd, XATTR_RESOURCEFORK_NAME, 0);
+        fsetxattr(fd, XATTR_RESOURCEFORK_NAME, buf, sz, 0, 0);
+        free(buf);
+    }
+}
+
+size_t MacResourceFork::read(size_t offset, void *p, size_t n)
+{
+    return fgetxattr(fd, XATTR_RESOURCEFORK_NAME, p, n, offset, 0);
+}
+
+size_t MacResourceFork::write(size_t offset, void *p, size_t n)
+{
+    int success = fsetxattr(fd, XATTR_RESOURCEFORK_NAME, p, n, offset, 0);
+    if(success < 0)
+        throw OSErrorException(ioErr);
+    return n;
 }
 
 ItemPtr MacItemFactory::createItemForDirEntry(ItemCache& itemcache, CNID parID, CNID cnid,
@@ -66,12 +101,11 @@ void MacItemFactory::createFile(const fs::path& parentPath, mac_string_view name
 
 std::unique_ptr<OpenFile> MacFileItem::open()
 {
-    return std::make_unique<PlainDataFork>(path_);
+    return std::make_unique<PlainDataFork>(path());
 }
 std::unique_ptr<OpenFile> MacFileItem::openRF()
 {
-    fs::path rsrc = path() / "..namedfork/rsrc";
-    return std::make_unique<MacResourceFork>(rsrc, PlainDataFork::create);
+    return std::make_unique<MacResourceFork>(path());
 }
 
 ItemInfo MacFileItem::getInfo()
