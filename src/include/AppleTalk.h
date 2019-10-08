1338965be1452415cf73c1df223275e42be0aad5
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2019-10-09 00:28:15 +0200

    remove hand-written headers

--- src/include/AppleTalk.h
+++
@@ -1,9 +0,0 @@
-#include <ExMacTypes.h>
-#include <base/lowglobals.h>
-
-namespace Executor
-{
-    const LowMemGlobal<Byte> PortBUse { 0x291 }; // AppleTalk IMII-305 (true-b);
-    const LowMemGlobal<Ptr> ABusVars { 0x2D8 }; // AppleTalk IMII-328 (false);
-    const LowMemGlobal<Ptr> ABusDCE { 0x2DC }; // AppleTalk MPW (false);
-}
