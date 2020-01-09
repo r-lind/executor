eb53030b8eefd8555d672ab0f0ae0651b6167177
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2020-01-09 23:42:47 +0100

    add test cases for correct behaviour of PostEvent & modifier keys

---
+++ tests/events.cpp
@@ -0,0 +1,91 @@
+#include "gtest/gtest.h"
+
+#include "compat.h"
+
+#ifdef EXECUTOR
+#include <ToolboxEvent.h>
+#include <OSEvent.h>
+#else
+#include <Events.h>
+#endif
+
+TEST(Events, FlushThenGet)
+{
+    FlushEvents(everyEvent, 0);
+
+    EventRecord e;
+    bool gotEvent = GetOSEvent(everyEvent, &e);
+    long ticks = TickCount();
+
+    EXPECT_FALSE(gotEvent);
+    EXPECT_EQ(nullEvent, e.what);
+    EXPECT_LE(e.when, ticks);
+    EXPECT_GT(e.when + 5, ticks);
+}
+
+
+TEST(Events, PostKey)
+{
+    FlushEvents(everyEvent, 0);
+
+    PostEvent(keyDown, 0 | 'a');
+
+    EventRecord e;
+    bool gotEvent = GetOSEvent(everyEvent, &e);
+    long ticks = TickCount();
+
+    EXPECT_TRUE(gotEvent);
+    EXPECT_EQ(keyDown, e.what);
+    EXPECT_LE(e.when, ticks);
+    EXPECT_GT(e.when + 5, ticks);
+    EXPECT_EQ(0 | 'a', e.message);
+    EXPECT_EQ(btnState, e.modifiers & 0xFF80);
+
+    EXPECT_EQ(0, ((uint8_t*)0x174)[0]);
+    PostEvent(keyUp, 0 | 'a');
+
+
+    gotEvent = GetOSEvent(everyEvent, &e);
+    ticks = TickCount();
+
+    EXPECT_FALSE(gotEvent);
+    EXPECT_EQ(nullEvent, e.what);
+    EXPECT_LE(e.when, ticks);
+    EXPECT_GT(e.when + 5, ticks);
+}
+
+
+
+TEST(Events, PostKeyMod)
+{
+    FlushEvents(everyEvent, 0);
+
+        // set command key in KeyMap
+    ((uint8_t*)0x174)[6] |= 0x80;
+
+    PostEvent(keyDown, 0 | 'a');
+
+        // clear command key in KeyMap
+    ((uint8_t*)0x174)[6] &= ~0x80;
+
+    EventRecord e;
+    bool gotEvent = GetOSEvent(everyEvent, &e);
+    long ticks = TickCount();
+
+    EXPECT_TRUE(gotEvent);
+    EXPECT_EQ(keyDown, e.what);
+    EXPECT_LE(e.when, ticks);
+    EXPECT_GT(e.when + 5, ticks);
+    EXPECT_EQ(0 | 'a', e.message);
+    EXPECT_EQ(btnState | cmdKey, e.modifiers & 0xFF80);
+
+    EXPECT_EQ(0, ((uint8_t*)0x174)[0] & 0x80);
+
+    gotEvent = GetOSEvent(everyEvent, &e);
+    ticks = TickCount();
+
+    EXPECT_FALSE(gotEvent);
+    EXPECT_EQ(nullEvent, e.what);
+    EXPECT_LE(e.when, ticks);
+    EXPECT_GT(e.when + 5, ticks);
+}
